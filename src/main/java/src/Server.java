5eb9027ef7d88009d0e606842c4380ebd7d077d9
Marta Ramírez González <32357347+chainwarden16@users.noreply.github.com>
2018-05-16 12:13:40 +0200

    Subida de código
    Al parecer, GitHub no me permite subir código mediante la aplicación de escritorio, así que intentaré subirlo por aquí directamente.

--- src/main/java/src/Server.java
+++ src/main/java/src/Server.java
@@ -1,5 +1,21 @@
 package src;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.sqlite.*;
+
+import com.sun.xml.internal.messaging.saaj.util.Base64;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import src.impl.ServerSocketImpl;
@@ -8,31 +24,106 @@ import src.utils.GenericSocket.GenericSocketException;
 
 public class Server implements Runnable {
 
-  @Override
+  @SuppressWarnings("unchecked")
+@Override
 	public void run() {
         // Ejecución del servidor
         ServerSocketImpl serverSocket = null;
         Main.log.debug("Inicio de servidor");
         try {
-            serverSocket = new ServerSocketImpl(4430);
-
+            serverSocket = new ServerSocketImpl(8080);
+            
             while(true) {
                 serverSocket.awaitConnection();
                 JSONObject message = serverSocket.readLineFromSocket();
                 // Poner aqui restricciones necesarias
+                
+                JSONArray data = (JSONArray) message.get("data");
+                Signature signature = (Signature) message.get("signature");
+                String publicKeyEnc = (String) message.get("publicKey");
+                
+                System.out.println(data);
+                System.out.println(signature);
+                System.out.println(publicKeyEnc);
+                
+                Signature sigVer = Signature.getInstance("SHA256withRSA","SC");
+                String publicKeyString = Base64.base64Decode(publicKeyEnc);
+                PublicKey publicKey = PublicKey.class.cast(publicKeyString);
+                sigVer.initVerify(publicKey);
+                Boolean flag1 = sigVer.verify(signature.toString().getBytes());
+                
+                //Comprobamos los datos introducidos
+                Boolean mayorQueCero = false;
+                Boolean valido = true;
+                for(int ind=0;ind<=data.size();ind++){
+                	
+                	//Tomamos el valor que haya en el campo y miramos si es distinto de cero
+                	int valor = (int) data.get(ind);
+                	
+                	System.out.println("El valor de valor es "+valor);
+                	
+                	if(valor<0 || valor > 300){
+                		
+                		valido = false;
+                		System.out.println("Valido es: "+valido);
+                		break;
+                		
+                	}
+                	
+                	if(!mayorQueCero){
+                		
+                		System.out.println("mayorQueCero es: "+mayorQueCero);
+                		mayorQueCero = valor>0;
+                		
+                	}
+                		
+                }
+                
+                
+                
                 // ...
                 // y guardado en BD
                 // ...
                 // Si todo sale bien, enviar esto:
                 JSONObject result = new JSONObject();
-                result.put("status", "Success");
+                
+                //Si hay algun valor mayor que cero, si todos los valores estan entre 0 y 300 y si la firma es
+                //correcta, entonces se guarda un success. Si no, se guarda un Incorrect, o lo que sea
+                
+                if(flag1 && valido && mayorQueCero){
+                	result.put("status", "Success");
+                	connect();
+                	result.put("s�banas", (int) data.get(0));
+                	result.put("camas", (int) data.get(1));
+                	result.put("mesas", (int) data.get(2));
+                	result.put("sillas", (int) data.get(3));
+                	result.put("minibar", (int) data.get(4));
+                	String saveResult = result.toString();
+                	insertData(saveResult);
+                	
+                }else{
+                	
+                	result.put("status", "Failure");	
+                }
                 System.out.println(result.toJSONString());
                 serverSocket.writeLineToSocket(result.toJSONString());
                 serverSocket.closeClientSocket();
             }
         } catch(GenericSocketException gse) {
             gse.printStackTrace();
-        } finally {
+        } catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchProviderException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SignatureException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
             try {
                 if(serverSocket != null) {
                     serverSocket.closeSocket();
@@ -44,4 +135,28 @@ public class Server implements Runnable {
         }
 	}
 
+  private static Connection connect() {
+      // SQLite connection string
+      String url = "jdbc:sqlite:C://sqlite/db/AndroidRequestServer.db";
+      Connection conn = null;
+      try {
+          conn = DriverManager.getConnection(url);
+      } catch (SQLException e) {
+          System.out.println(e.getMessage());
+      }
+      return conn;
+  }
+  
+  public void insertData(String name) {
+      String sql = "INSERT INTO resources(json) VALUES(?,?)";
+
+      try (Connection conn = Server.connect();
+              PreparedStatement pstmt = conn.prepareStatement(sql)) {
+          pstmt.setString(1, name);
+          pstmt.executeUpdate();
+      } catch (SQLException e) {
+          System.out.println(e.getMessage());
+      }
+  }
+  
 }
