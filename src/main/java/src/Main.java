c547a4877877f1d313702ad1485cc3d5661f57fe
Marta Ramírez González <32357347+chainwarden16@users.noreply.github.com>
2018-05-16 12:17:41 +0200

    Subida de código
    Parece que GitHub no me permite subir código de forma formal, así que lo subiré por aquí directamente.

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -1,5 +1,11 @@
 package src;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
@@ -20,6 +26,41 @@ public class Main {
         } catch(InterruptedException ie) {
             Main.log.warn(ie.getMessage());
             throw ie;
+        }
+    }
+    
+    public static void createNewDatabase(String fileName) {
+    	 
+        String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
+ 
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+ 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/AndroidRequestServer.db";
+        
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS resources (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL\n"
+                + ");";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
